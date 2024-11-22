ee2344b9c48d705648a4c9a95ac6be2e0a839aab
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-11-22 18:50:57 +0100

    fix: translatable settings

--- settings.ts
+++ settings.ts
@@ -15,119 +15,139 @@ export default [
     group: 'default',
     type: SettingType.text,
     value: 'llama3.2', // Default model
+    translatable: false,
   },
   {
     label: 'keep_alive',
     group: 'default',
     type: SettingType.text,
     value: '5m', // Default value for keeping the model in memory
+    translatable: false,
   },
   {
     label: 'max_messages_ctx',
     group: 'default',
     type: SettingType.number,
     value: 5, // Default number of messages to retrieve for context
+    translatable: false,
   },
   {
     label: 'context',
     group: 'default',
     type: SettingType.text,
     value: `You are an AI Assistant that works for Hexastack, the IT company behind Hexabot the chatbot builder.`, // Default value for keeping the model in memory
+    translatable: true,
   },
   {
     label: 'instructions',
     group: 'default',
     type: SettingType.textarea,
     value: `Answer the user QUESTION using the DOCUMENTS text above. Keep your answer ground in the facts of the DOCUMENT. If the DOCUMENT doesn’t contain the facts to answer the QUESTION, apologize and try to give an answer that promotes the company and its values. DO NOT SAY ANYTHING ABOUT THESE DOCUMENTS, nor their EXISTENCE.`,
+    translatable: false,
   },
   {
     label: 'fallback_message',
     group: 'default',
     type: SettingType.textarea,
     value: `Something went wrong ... please try again later.`,
+    translatable: false,
   },
   {
     label: 'mirostat',
     group: 'options',
     type: SettingType.number,
     value: 0, // Default: disabled
+    translatable: false,
   },
   {
     label: 'mirostat_eta',
     group: 'options',
     type: SettingType.number,
     value: 0.1, // Default value
+    translatable: false,
   },
   {
     label: 'mirostat_tau',
     group: 'options',
     type: SettingType.number,
     value: 5.0, // Default value
+    translatable: false,
   },
   {
     label: 'num_ctx',
     group: 'options',
     type: SettingType.number,
     value: 2048, // Default value
+    translatable: false,
   },
   {
     label: 'repeat_last_n',
     group: 'options',
     type: SettingType.number,
     value: 64, // Default value
+    translatable: false,
   },
   {
     label: 'repeat_penalty',
     group: 'options',
     type: SettingType.number,
     value: 1.1, // Default value
+    translatable: false,
   },
   {
     label: 'temperature',
     group: 'options',
     type: SettingType.number,
     value: 0.8, // Default value
+    translatable: false,
   },
   {
     label: 'seed',
     group: 'options',
     type: SettingType.number,
     value: 0, // Default value
+    translatable: false,
   },
   {
     label: 'stop',
     group: 'options',
     type: SettingType.text,
     value: 'AI assistant:', // Default stop sequence
+    translatable: true,
   },
   {
     label: 'tfs_z',
     group: 'options',
     type: SettingType.number,
     value: 1, // Default value, 1.0 means disabled
+    translatable: false,
   },
   {
     label: 'num_predict',
     group: 'options',
     type: SettingType.number,
     value: 20, // Default value
+    translatable: false,
   },
   {
     label: 'top_k',
     group: 'options',
     type: SettingType.number,
     value: 40, // Default value
+    translatable: false,
   },
   {
     label: 'top_p',
     group: 'options',
     type: SettingType.number,
     value: 0.9, // Default value
+    translatable: false,
   },
   {
     label: 'min_p',
     group: 'options',
     type: SettingType.number,
     value: 0.0, // Default value
+    translatable: false,
   },
 ] as const satisfies PluginSetting[];
